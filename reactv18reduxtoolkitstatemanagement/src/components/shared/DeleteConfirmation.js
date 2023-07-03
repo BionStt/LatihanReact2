4bf4a6bbdcf0cb68324f1ff6a8bc45248f36cde6
Sutanto Gasali <43228630+BionStt@users.noreply.github.com>
2023-07-03 23:28:30 +0700

    add rect crud json server

---
+++ reactv18reduxtoolkitstatemanagement/src/components/shared/DeleteConfirmation.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+export default function DeleteConfirmation(props) {
+  return (
+    <>
+      <Modal
+        show={props.showModal}
+        onHide={() => {
+          props.hideDeleteModalHandler();
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.body}</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              props.hideDeleteModalHandler();
+            }}
+          >
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              props.confirmDeleteHandler();
+            }}
+          >
+            Confirm Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
